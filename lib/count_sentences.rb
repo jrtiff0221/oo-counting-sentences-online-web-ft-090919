966a6214c1ba5d79e0f723ce8f5de23c080ac094
Johnnae Tiffany <jrtiff0221@gmail.com>
2019-09-19 03:24:24 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,8 +15,7 @@ class String
   end
 
   def count_sentences
-    # sentences = self.split(/\w(\!|\?|\.)/)
-    sentences = self.split(".").split("?").split('!')
+    sentences = self.split(/(\!|\?|\.)/)
     puts "\nsentences"
     puts sentences
     
